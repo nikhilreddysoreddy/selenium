0917fddc0091140aedefd0543ba696071ee5a1b0
nikhilreddysoreddy <118904318+nikhilreddysoreddy@users.noreply.github.com>
2022-11-23 23:03:25 +0530

    Update TestRunner.java

--- project1/src/test/java/stepDefinition/TestRunner.java
+++ project1/src/test/java/stepDefinition/TestRunner.java
@@ -3,7 +3,7 @@ package stepDefinition;
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
-/*@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @cucumberoptions(features="src/test/resources/features2/new_tours2.feature",
 glue= {"stepDefenation"},
 monochrome=true,
@@ -11,7 +11,7 @@ plugin= {"pretty","html:target/htmlreports/report.html",
 		"json:target/jsonreports/report.json",
 		"junit:target/junitreports/report.xml"
 }
-)*/
+)
 
 public class TestRunner {
 	
